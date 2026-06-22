4ee97da9fcebf6f53b71fce1630151e9bc32d0bc
snehithpanda007-lgtm <snehithpanda007@gmail.com>
2026-06-22 22:17:44 +0530

    Implemented Next.js

---
+++ app/notes/page.tsx
@@ -0,0 +1,8 @@
+export default function NotesPage() {
+  return (
+    <main>
+      <h1>Notes</h1>
+      <p>Store and organize your notes.</p>
+    </main>
+  );
+}
---
+++ app/tasks/page.tsx
@@ -0,0 +1,8 @@
+export default function TasksPage() {
+  return (
+    <main>
+      <h1>Tasks</h1>
+      <p>Manage your tasks here.</p>
+    </main>
+  );
+}
